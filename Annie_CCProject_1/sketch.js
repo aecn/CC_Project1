71bf8dff9c1e4a2f551dfbe800c0f148fdf95c33
annie <ac8502@nyu.edu>
2021-03-21 00:08:12 -0500

    second push
    I referenced the index.html to the .js file, and the main image is coded

--- Annie_CCProject_1/sketch.js
+++ Annie_CCProject_1/sketch.js
@@ -1,5 +1,7 @@
+// Annie Chen - Project 1
+
 function setup() {
-  createCanvas(1000, 800);
+  createCanvas(1000, 750);
   background("#d8e2dc");
 }
 
@@ -20,14 +22,31 @@ function draw() {
   arc(500, 385, 120, 130, 0, PI, CHORD); // shirt collar line
   
   fill("#d4a373");
-  arc(500, 100, 400, 200, PI, 2*PI, CHORD); // hair
+  arc(500, 100, 380, 180, PI, 2*PI, CHORD); // hair
   
   fill("#ffdab9");
-  arc(500, 100, 400, 500, 0, PI, CHORD); // head
+  arc(500, 100, 380, 500, 0, PI, CHORD); // head
   
   noStroke();
   line(450, 340, 450, 420); // left neck line
   line(550, 340, 550, 420); // right neck line
   rect(450, 330, 100, 80); // middle neck
+
+  fill("#000000");
+  ellipse(400, 200, 30, 30); // left eye
+  ellipse(600, 200, 30, 30); // right eye
+  fill("#ffffff");
+  ellipse(410, 205, 10, 10); // left eye highlight
+  ellipse(590, 205, 10, 10); // right eye highlight
+
+  fill("#ffd6ba");
+  stroke("#c18c5d");
+  triangle(500, 200, 475, 250, 525, 250); // nose
+
+  noFill();
+  stroke("#c18c5d");
+  arc(500, 275, 120, 80, 0, PI, OPEN); // mouth
+
+
   
 }
